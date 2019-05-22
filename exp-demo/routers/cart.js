79649753bebf35faf2efe49cc58d1cd48032115f
yangdecai1111 <461897887@qq.com>
2019-05-22 10:55:20 +0800

    master

--- exp-demo/routers/cart.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-
-router.get("/cart",(req,res)=>{
-    res.send("购物车有东西了")
-})
-
-module.exports=router;
--- exp-demo/routers/good.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-
-router.get("/good",(req,res)=>{
-    res.send("添加商品了");
-})
-
-module.exports=router;
